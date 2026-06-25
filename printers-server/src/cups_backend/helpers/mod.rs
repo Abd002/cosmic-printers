92b6c375c39e2e21f8bc3afc9bf78c997f97081d
abd002 <abdelrahman.5alifa@gmail.com>
2026-06-25 10:25:35 +0300

    perf(printers): parallelize printer attribute loading

--- printers-server/src/cups_backend/helpers/mod.rs
+++ printers-server/src/cups_backend/helpers/mod.rs
@@ -5,9 +5,7 @@ mod identity;
 mod ipp;
 mod options;
 
-pub(super) use attributes::{
-    PRINTER_ATTRIBUTES, fill_attrs_from_device, fill_device_attrs_from_device, fill_missing_attrs,
-};
+pub(super) use attributes::{PRINTER_ATTRIBUTES, fill_attrs_from_device, fill_missing_attrs};
 pub(super) use destinations::{configured_printers, discovered_printers};
 pub(super) use identity::{printer_queue_name, printers_match, split_queue_instance};
 pub(super) use ipp::{CupsResultExt, LocalSocketGuard, add_requesting_user, ensure_success};
